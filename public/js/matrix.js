1fadecbc47a02b1c8eaadc9ee6294fa49cc5ca61
shubhammuley <shubham.muley@rapidops.com>
2020-03-11 18:43:33 +0530

    update

--- public/js/matrix.js
+++ public/js/matrix.js
@@ -4,6 +4,57 @@ function mapdata() {
   let edges = [];
 
   // Write your logic here...
+  let matrix = 3;
+
+
+for( let i=0 ; i<matrix ; i++){
+  for(let j=0; j<matrix ; j++){
+    let obj={"data":{"id":`N${i}-${j}`}};
+    nodes.push(obj);
+    
+    if( j < matrix-1 ){
+      let obj = {"data":{"id":`${i}-${j}E`,"source" :`N${i}-${j}` ,'target':`N${i}-${j+1}`}}
+
+       edges.push(obj);
+    }
+
+    if( j>0 ){
+      let obj = {"data":{"id":`${i}-${j}W`,"source" :`N${i}-${j}` ,'target':`N${i}-${j-1}`}}
+       edges.push(obj);
+    }
+    if(i < matrix-1){
+      let obj = {"data":{"id":`${i}-${j}S`,"source" :`N${i}-${j}` ,'target':`N${i+1}-${j}`}}
+       edges.push(obj);
+    }
+    if(i >0){
+      let obj = {"data":{"id":`${i}-${j}N`,"source" :`N${i}-${j}` ,'target':`N${i-1}-${j}`}}
+       edges.push(obj);
+    }
+    
+    if( i<matrix-1 && j<matrix-1){
+      let obj = {"data":{"id":`${i}-${j}A`,"source" :`N${i}-${j}` ,'target':`N${i+1}-${j+1}`}}
+       edges.push(obj);
+    }
+    
+    if( j > 0 && i < matrix-1 ){
+      let obj = {"data":{"id":`${i}-${j}B`,"source" :`N${i}-${j}` ,'target':`N${i+1}-${j-1}`}}
+        edges.push(obj);
+    }
+
+  }
+
+ 
+
+}
+
+console.log(edges.length);
+console.log(nodes.length);
+
+
+
+
+
+
 
   elements = {
     nodes,
